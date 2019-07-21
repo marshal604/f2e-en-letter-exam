23487c73dfd07fa39e75b36de412962d1f9f4338
Lerdor <marshal604@gmail.com>
2019-07-21 10:25:04 +0800

    feat: add canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from '@auth/auth-guard.service';
 
 const routes: Routes = [
-  { path: 'exam', loadChildren: '@exam/exam.module.ts#ExamModule' },
-  { path: 'config', loadChildren: '@config/config.module.ts#ConfigModule' }
+  {
+    path: 'exam',
+    loadChildren: '@exam/exam.module.ts#ExamModule',
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'config',
+    loadChildren: '@config/config.module.ts#ConfigModule',
+    canActivate: [AuthGuardService]
+  }
 ];
 @NgModule({
   imports: [
--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '@core/services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
-  constructor() {}
+export class AuthGuardService implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return !!this.userService.getUserInfo();
+  }
 }
