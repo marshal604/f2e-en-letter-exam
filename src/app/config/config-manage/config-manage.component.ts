3eb70879d7ab861849548f91ea85a651759b03c3
Lerdor <marshal604@gmail.com>
2019-07-22 00:18:36 +0800

    feat: init header, headerMap to table

--- src/app/config/config-manage/config-manage.component.ts
+++ src/app/config/config-manage/config-manage.component.ts
@@ -1,12 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ExamQuestionBankInfo } from '@exam/exam.model';
+import { ExamService } from '@core/services/exam/exam.service';
+
 @Component({
   selector: 'yur-config-manage',
   templateUrl: './config-manage.component.html',
   styleUrls: ['./config-manage.component.css']
 })
 export class ConfigManageComponent implements OnInit {
-  constructor() {}
+  examQuestionBankList: ExamQuestionBankInfo[] = [];
+  examQuestionHeaders: string[];
+  examQuestionHeadersMap: Map<string, string | number>;
+  constructor(private examService: ExamService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initExamQuestionBankList();
+    this.initQuestionHeaders();
+    this.initQuestionHeadersMap();
+  }
+
+  private initExamQuestionBankList() {
+    this.examService.queryExamQuestionList().then(data => {
+      this.examQuestionBankList = data;
+    });
+  }
+
+  private initQuestionHeaders() {
+    this.examQuestionHeaders = [
+      'no',
+      'name',
+      'examTime',
+      'examQuestionDisplayCount',
+      'examQuestionType',
+      'examPassScore',
+      'examAllowCount'
+    ];
+  }
+  private initQuestionHeadersMap() {
+    this.examQuestionHeadersMap = new Map([
+      ['no', 'No'],
+      ['name', '題庫'],
+      ['examTime', '考題時間'],
+      ['examQuestionDisplayCount', '每題題數'],
+      ['examQuestionType', '考題模式'],
+      ['examPassScore', '及格分數'],
+      ['examAllowCount', '考試上限次數']
+    ]);
+  }
 }
