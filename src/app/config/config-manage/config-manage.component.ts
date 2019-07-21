ef353e08e99919805f81e4df9d139e8f1ae479ab
Lerdor <marshal604@gmail.com>
2019-07-21 16:33:35 +0800

    feat: init configManage, configTable

---
+++ src/app/config/config-manage/config-manage.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yur-config-manage',
+  templateUrl: './config-manage.component.html',
+  styleUrls: ['./config-manage.component.css']
+})
+export class ConfigManageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/config/config-table/config-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yur-config-table',
+  templateUrl: './config-table.component.html',
+  styleUrls: ['./config-table.component.scss']
+})
+export class ConfigTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
