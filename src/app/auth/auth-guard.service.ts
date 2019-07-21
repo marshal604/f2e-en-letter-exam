2225bd9229af6a40c12073003a8ee494cf239bc9
Lerdor <marshal604@gmail.com>
2019-07-21 10:34:20 +0800

    feat: add permission condition

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -8,17 +8,31 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserService } from '@core/services/user.service';
+import { UserRole } from '@gql-models/auth/auth.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
+  permissionPath = ['config'];
   constructor(private userService: UserService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    return !!this.userService.getUserInfo();
+    const userInfo = this.userService.getUserInfo();
+    if (!userInfo) {
+      return false;
+    }
+
+    if (
+      this.permissionPath.includes(route.routeConfig.path) &&
+      userInfo.role > UserRole.Administrator
+    ) {
+      return false;
+    }
+
+    return true;
   }
 }
